42758055696937c534e25ec19111a7830d0fdfa7
Kieran Emery <kizwiz@hotmail.co.uk>
2023-11-06 15:47:15 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace RealEstate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<RealEstate> list = new List<RealEstate>();
+
+            int inputsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputsCount; i++)
+            {
+                string city = Console.ReadLine();
+                string street = Console.ReadLine();
+                int roomsCount = int.Parse(Console.ReadLine());
+                double price = double.Parse(Console.ReadLine());
+
+                RealEstate re = new RealEstate(city, street, roomsCount, price);
+                list.Add(re);
+            }
+
+            string filterCity = Console.ReadLine();
+
+            foreach (RealEstate re in list)
+            {
+                if (re.City == filterCity)
+                {
+                    Console.WriteLine(re.Street + " " + re.RoomsCount + " " + re.Price + " £");
+                }
+            }
+        }
+    }
+}
---
+++ RealEstate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealEstate
+{
+    internal class RealEstate
+    {
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int RoomsCount { get; set; }
+        public double Price { get; set; }
+
+        public RealEstate(string city, string street, int roomsCount, double price)
+        {
+            City = city;
+            Street = street;
+            RoomsCount = roomsCount;
+            Price = price;
+        }
+    }
+}
